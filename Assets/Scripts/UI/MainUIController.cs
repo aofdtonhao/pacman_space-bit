8aa4873ae109b19d0c6c491d841bcbb0be728b57
tonhex <aofdtonhao@gmail.com>
2022-02-05 21:50:38 -0300

    configurando pacotes

---
+++ Assets/Scripts/UI/MainUIController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MainUIController : MonoBehaviour
+{
+
+    void Awake() {
+        Debug.Log("Awake");    
+    }
+
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    void Update()
+    {
+        Debug.Log("Update");
+    }
+
+}
