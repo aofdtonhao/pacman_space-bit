23bd3f7752df7dfa4f312404a9202ec7c550f7ae
tonhex <aofdtonhao@gmail.com>
2022-02-07 22:45:54 -0300

    criando prefabs

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,51 +1,54 @@
 using UnityEngine;
 
-namespace Tonhex
+public class AudioManager : MonoBehaviour
 {
 
-    public class AudioManager : MonoBehaviour
+    public enum Teste : int
     {
+        OPA,
+        SEI,
+        TODO
+    }
 
-        public static AudioManager instance { get; private set; } = null;
+    public static AudioManager instance { get; private set; } = null;
 
-        [SerializeField]
-        private AudioSource musicAudioSource;
-        [SerializeField]
-        private AudioSource effectAudioSource;
+    [SerializeField]
+    private AudioSource musicAudioSource;
+    [SerializeField]
+    private AudioSource effectAudioSource;
 
-        [SerializeField]
-        private float lowPitchRange = 0.95f;
-        [SerializeField]
-        private float highPitchRange = 1.05f;
+    [SerializeField]
+    private float lowPitchRange = 0.95f;
+    [SerializeField]
+    private float highPitchRange = 1.05f;
 
-        void Awake()
-        {
-            if (instance == null) {
-                instance = this;
-            } else if (instance != this) {
-                Destroy(gameObject);
-            }
+    [SerializeField]
+    private AudioClip[] audioClips = new AudioClip[System.Enum.GetValues(typeof(Teste)).Length];
 
-            DontDestroyOnLoad(gameObject);
+    void Awake()
+    {
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(gameObject);
         }
 
-        public void PlayEffect(AudioClip clip)
-        {
-            effectAudioSource.pitch = 1f;
-            effectAudioSource.clip = clip;
-            effectAudioSource.Play();
-        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlayEffect(Teste clip, float pitch = 1f)
+    {
+        effectAudioSource.pitch = pitch;
+        effectAudioSource.clip = audioClips[(int)clip];
+        effectAudioSource.Play();
+    }
 
 
-        public void PlayEffectRandomPitch(AudioClip clip)
-        {
-            float randomPitch = Random.Range(lowPitchRange, highPitchRange);
+    public void PlayEffectRandomPitch(Teste clip)
+    {
+        float randomPitch = Random.Range(lowPitchRange, highPitchRange);
 
-            effectAudioSource.pitch = randomPitch;
-            effectAudioSource.clip = clip;
-            effectAudioSource.Play();
-        }
-
+        PlayEffect(clip, randomPitch);
     }
 
 }
